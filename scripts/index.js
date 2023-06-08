46fab1ed440b039640473bba0eb630f9d74e3874
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-08 10:35:31 -0400

    Add fix bugs in details page

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,80 @@
 "use strict";
 
+
+//tbody element defined by html element id 
 const tbody = document.getElementById("courseDetails");
 
 
 
+//arrow function , when window finish loading execute function 
+window.onload = () => {
+    console.log("First console Window load");
 
-window.onload = init;
-
-function init() {
-    showCourses();
+    //show all courses 
+    showAllCourses();
 }
 
-function showCourses() {
-    console.log("clicked");
 
-    let theUrl = "http://localhost:8081/api/courses";
-    fetch(theUrl)
+//function to show all courses
+function showAllCourses() {
+
+    console.log("second console this is show all courses function");
+
+
+    //fetch method , url to get api with courses
+    fetch("http://localhost:8081/api/courses")
+
         .then((response) => response.json())
+
         .then((courses) => {
             console.log(courses);
 
+            //for loop, used to loop through values starting at index
             for (let i = 0; i < courses.length; i++) {
 
-                let row = tbody.insertRow(-1);
-
-
-                let cell1 = row.insertCell(0);
-                cell1.className = "text-center px-2";
-                cell1.innerHTML = courses[i].courseName;
-
-                let cell2 = row.insertCell(1);
-                cell2.className = "text-center px-2";
-                cell2.innerHTML = courses[i].dept; cell2.className = "text-center px-2";
-                cell2.innerHTML = courses[i].dept;
-
-                let cell3 = row.insertCell(2);
-                cell3.className = "text-center mx-5";
-                cell3.innerHTML = courses[i].courseNum + "/";
-
-                let anchor = document.createElement("a");
-                anchor.href = `http://localhost:8081/details.html?courseid=${courses[i].id}`;
-                // anchor.className = "col-6";
-                // anchor.className = "container-fluid";
-
-
-                cell3.appendChild(anchor);
-                anchor.text = courses[i].courseName;
-
-
-
+                buildRowForCourse(courses[i]);
 
             }
         });
+}
+
+function buildRowForCourse(course) {
+
+    //define row to be inside table body for each course
+    let row = tbody.insertRow(-1);
+
+    //method insert new td into table row
+    let cell1 = row.insertCell(0);
+    cell1.className = "text-center px-1";
+    cell1.innerHTML = course.courseName;
+
+    let cell2 = row.insertCell(1);
+    cell2.className = "text-center px-1";
+    cell2.innerHTML = course.dept;
+
+
+    // let cell3 = row.insertCell(2);
+    // cell3.className = "text-center mx-5";
+    // cell3.innerHTML = courses[i].courseNum + "/";
+
+
+    let cell3 = row.insertCell(2);
+    cell3.className = "text-center mx-3";
+    cell3.innerHTML = course.courseNum;
+
+    let cell4 = row.insertCell();
+    cell4.className = "text-center mx-3";
+    cell4.innerHTML = "";
+
+    let anchor = document.createElement("a");
+    anchor.href = `details.html?courseid=${course.id}`;
+    anchor.text = course.courseName;  
+    
+    // anchor.className = "col-6";
+    // anchor.className = "container-fluid";
+
+    // const coursesLink = document.getElementById("coursesLink");
+    cell4.appendChild(anchor);
 
 
 
